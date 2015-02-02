3a33cdda42d87482889f8268885d0c0544c77ccb
Jarek Radosz <jradosz@gmail.com>
2015-02-02 17:15:09 +0100

    Fix specs on ruby 1.8.7.

--- spec/api_client/base_spec.rb
+++ spec/api_client/base_spec.rb
@@ -60,7 +60,10 @@ describe ApiClient::Base do
   describe "strict_read" do
     it "fails if the key is missing and strict_read is set" do
       api = StrictApi.new
-      lambda { api.missing }.should raise_error(KeyError)
+      lambda { api.missing }.should raise_error do |error|
+        error_type = error.class.to_s
+        error_type.should match(/KeyError|IndexError/)
+      end
     end
 
     it "doesn't fail if strict_read is not set" do
@@ -87,12 +90,18 @@ describe ApiClient::Base do
 
     it "calling method which asks for missing attribute fails" do
       api = StrictApi.new
-      lambda { api.accessor_of_x }.should raise_error(KeyError)
+      lambda { api.accessor_of_x }.should raise_error do |error|
+        error_type = error.class.to_s
+        error_type.should match(/KeyError|IndexError/)
+      end
     end
 
     it "passes strict api configuration to subclasses" do
       api = StrictDescendant.new
-      lambda { api.missing }.should raise_error(KeyError)
+      lambda { api.missing }.should raise_error do |error|
+        error_type = error.class.to_s
+        error_type.should match(/KeyError|IndexError/)
+      end
     end
   end
 end
